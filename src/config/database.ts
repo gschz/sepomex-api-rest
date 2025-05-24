0742e8052c3a5d55b971ed05acde4026e95b2db8
Gera SSG <gxra.dev@gmail.com>
2025-05-24 14:19:37 -0700

    refactor(db): adjust database configuration

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,17 +3,17 @@
  * configuración de la conexión a la base de datos
  */
 
-import pg from "pg";
-import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from "./config.js";
+import pg from 'pg';
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from './config';
 const { Pool } = pg;
-import type { DbConfig } from "../types/index.js";
+import type { DbConfig } from '@/types';
 
 const config: DbConfig = {
-	user: DB_USER,
-	password: DB_PASSWORD,
-	host: DB_HOST,
-	port: DB_PORT ? Number.parseInt(DB_PORT) : undefined,
-	database: DB_NAME,
+   user: DB_USER,
+   password: DB_PASSWORD,
+   host: DB_HOST,
+   port: Number.parseInt(DB_PORT),
+   database: DB_NAME,
 };
 
 export const pool = new Pool(config);
